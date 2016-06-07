357eee469023b28ac740a06a87e93050d58b2cdd
hayovanloon <hayo.vanloon@incentro.com>
2016-06-07 21:27:47 +0200

    improve bash scripts

--- sql2gcloud.sh
+++ sql2gcloud.sh
@@ -1,1 +1,7 @@
-java -cp "*" nl.hayovanloon.gcp.sql2gcloud.Runner $@
+LOG_CONF="log4j2.xml"
+
+if [[ -z "$LOG_CONF" ]]; then
+    java -Dlog4j.configurationFile="$LOG_CONF" -cp "*" nl.hayovanloon.gcp.sql2gcloud.Runner $@
+else 
+    java -cp "*" nl.hayovanloon.gcp.sql2gcloud.Runner $@
+fi
